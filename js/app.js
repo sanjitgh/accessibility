70191735f734c50820f37f67dea47ad524905152
Sanjit <sanjitkumarghosh0@gmail.com>
2024-09-06 17:27:20 +0600

    phd sec done

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// carousel
+$(document).ready(function () {
+  $(".phd_student_carousel").owlCarousel({
+    items: 4,
+    loop: true,
+    dots: false,
+    nav: true,
+    margin: 40,
+    navText: [
+      '<i class="fa-solid fa-chevron-left"></i>',
+      '<i class="fa-solid fa-chevron-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+
+      575: {
+        items: 2,
+      },
+
+      991: {
+        items: 3,
+      },
+
+      1025: {
+        items: 4,
+      },
+    },
+  });
+});
