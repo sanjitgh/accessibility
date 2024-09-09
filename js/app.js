d657d4043d974c2bb9a5ccc2f912b22c0b939369
Sanjit <sanjitkumarghosh0@gmail.com>
2024-09-09 21:30:44 +0600

    edit

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     loop: true,
     dots: false,
     nav: true,
-    margin: 40,
+    margin: 35,
     navText: [
       '<i class="fa-solid fa-chevron-left"></i>',
       '<i class="fa-solid fa-chevron-right"></i>',
@@ -38,7 +38,7 @@ $(document).ready(function () {
     loop: true,
     dots: false,
     nav: true,
-    margin:20,
+    margin:10,
     navText: [
       '<i class="fa-solid fa-chevron-left"></i>',
       '<i class="fa-solid fa-chevron-right"></i>',
